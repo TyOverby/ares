8c6ba01b89236714d8303761c9ac9a8a7d9f58ec
ben wolfson <wolfson@gmail.com>
2015-09-24 12:39:40 -0700

    fixes ' in tokenizer, load stdlib:
    also adds a "list" conversion function, and fixes the definition of
    for-each (which needs to evaluate its arguments---and probably shouldn't
    be a special form at all).

--- tests/list.rs
+++ tests/list.rs
@@ -10,3 +10,14 @@ fn build_list() {
     eval_ok!("(build-list (lambda (push) (push 1)))", vec![1]);
     eval_ok!("(build-list (lambda (push) (push 1) (push 2) (push 3)))", vec![1, 2, 3]);
 }
+
+#[test]
+fn to_list() {
+    eval_ok!("(list 1 2 3)", vec![1, 2, 3]);
+}
+
+#[test]
+fn test_map() {
+    eval_ok!("(map (list 1 2 3) (lambda (x) (+ x 1)))", vec![2, 3, 4]);
+    eval_ok!("(map '(1 2 3) (lambda (x) (+ x 1)))", vec![2, 3, 4]);
+}
