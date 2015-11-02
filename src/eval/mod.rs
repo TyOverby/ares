05892058e6416a58a2f2fbd7b586f930fb795ac0
Ty Overby <ty@pre-alpha.com>
2015-11-01 19:36:24 -0800

    Inline Foreign Function calls and Lambda calls

--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -10,24 +10,58 @@ mod foreign_function;
 mod procedure;
 mod context;
 
-#[derive(Clone, Eq, PartialEq, Debug)]
+#[derive(Clone)]
 pub enum StepState {
     EvalThis(Value, bool),
+    PopEnv,
     Return,
     Complete(Value),
     PreEvaluatedCallable {
         unevaluated: Vec<Value>
     },
-    Lambda {
+    ArgCollectingLambda {
         func: Procedure,
         evaluated: Vec<Value>,
         unevaluated: Vec<Value>
+    },
+    EvaluatingLambda {
+        name: Option<String>,
+        bodies: Vec<Value>,
     }
 }
 
-pub fn cleanup_stack<S: ?Sized + State>(prev_length: usize, ctx: &mut LoadedContext<S>) {
+fn cleanup_stack<S: ?Sized + State>(prev_length: usize, ctx: &mut LoadedContext<S>) {
     while ctx.stack.len() > (prev_length - 2) {
         ctx.stack.pop();
+    }
+}
+
+fn run_evaluation<S: ?Sized + State>(target_len: usize, ctx: &mut LoadedContext<S>) -> AresResult<(StepState, StepState)> {
+    let starting_len = target_len;
+    loop {
+        let cur_len = ctx.stack.len();
+        if cur_len < starting_len {
+            panic!("run_evaluation(..): stack is lower than should be possible.");
+        } else if cur_len == starting_len {
+            if let Some(&StepState::Complete(_)) = ctx.stack.last() {
+                break;
+            }
+        }
+
+        if let Err(e) = step_eval(ctx) {
+            cleanup_stack(starting_len, ctx);
+            return Err(e);
+        }
+    }
+
+    let top = ctx.stack.pop();
+    let next_top = ctx.stack.pop();
+    match (next_top, top) {
+        (Some(next_top), Some(top)) => {
+            Ok((next_top, top))
+        }
+        (next_top, top) =>
+            panic!("run_evaluation(..): invalid stack state [..., {:?}, {:?}]", next_top, top),
     }
 }
 
@@ -39,26 +73,24 @@ where S: State
     ctx.stack.push(StepState::Return);
     ctx.stack.push(StepState::EvalThis(value, false));
 
-    let len = ctx.stack.len();
-    if let Err(e) = step_eval(ctx) {
-        cleanup_stack(len, ctx);
-        return Err(e);
+    match try!(run_evaluation(ctx.stack.len(), ctx)) {
+        (StepState::Return, StepState::Complete(value)) => Ok(value),
+        (next_top, top) =>
+            panic!("eval(..): invalid stack state [{:?}, {:?}, {:?}]", ctx.stack, next_top, top),
     }
+}
 
-    while ctx.stack.len() > len {
-        if let Err(e) = step_eval(ctx) {
-            cleanup_stack(len, ctx);
-            return Err(e);
-        }
-    }
-
-    let result = ctx.stack.pop();
-    let ret = ctx.stack.pop();
-    match (ret, result) {
-        (Some(StepState::Return), Some(StepState::Complete(value))) => {
-            Ok(value)
-        }
-        (res, r) => panic!("eval(..): invalid stack state [..., {:?}, {:?}]", r, res),
+pub fn apply<'a, S: ?Sized>(func: &Value,
+                            args: Vec<Value>,
+                            ctx: &mut LoadedContext<S>) -> AresResult<Value>
+where S: State {
+    let cur_len = ctx.stack.len();
+    ctx.stack.push(StepState::Return);
+    try!(do_apply(func.clone(), args, ctx));
+    match try!(run_evaluation(cur_len + 2, ctx)) {
+        (StepState::Return, StepState::Complete(value)) => Ok(value),
+        (next_top, top) =>
+            panic!("apply(..): invalid stack state [{:?}, {:?}, {:?}]", ctx.stack, next_top, top),
     }
 }
 
@@ -70,7 +102,7 @@ fn step_eval<S: State + ?Sized>(ctx: &mut LoadedContext<S>) -> AresResult<()> {
                 let procedure = match value {
                     Value::Lambda(procedure, _) => procedure,
                     Value::ForeignFn(func) => {
-                        let apply_result = try!(apply_function(&func, unevaluated, ctx));
+                        let apply_result = try!(apply_function(func, unevaluated, ctx));
                         ctx.stack.push(StepState::Complete(apply_result));
                         return Ok(())
                     }
@@ -81,43 +113,60 @@ fn step_eval<S: State + ?Sized>(ctx: &mut LoadedContext<S>) -> AresResult<()> {
                 // so reverse this right now and call `pop` to get the next one.
                 unevaluated.reverse();
                 if let Some(first) = unevaluated.pop() {
-                    ctx.stack.push(StepState::Lambda {
+                    ctx.stack.push(StepState::ArgCollectingLambda {
                         func: procedure,
                         evaluated: vec![],
                         unevaluated: unevaluated,
                     });
                     ctx.stack.push(StepState::EvalThis(first, false));
                 } else {
-                    let returned = try!(apply_lambda(&procedure, vec![], ctx));
-                    ctx.stack.push(StepState::Complete(returned));
+                    try!(apply_lambda(procedure, vec![], ctx));
                 }
             }
-            StepState::Lambda { func, mut evaluated, mut unevaluated } => {
+            StepState::ArgCollectingLambda { func, mut evaluated, mut unevaluated } => {
                 evaluated.push(value);
                 if let Some(next) = unevaluated.pop() {
-                    ctx.stack.push(StepState::Lambda {
+                    ctx.stack.push(StepState::ArgCollectingLambda {
                         func: func,
                         evaluated: evaluated,
                         unevaluated: unevaluated,
                     });
                     ctx.stack.push(StepState::EvalThis(next, false));
                 } else {
-                    let returned = try!(apply_lambda(&func, evaluated, ctx));
-                    ctx.stack.push(StepState::Complete(returned));
+                    try!(apply_lambda(func, evaluated, ctx));
                 }
             }
-            a => panic!("step_eval(..): invalid stack state: [..., {:?}, {:?}]",
-                        a, StepState::Complete(value))
+            StepState::EvaluatingLambda { mut bodies, name } => {
+                if let Some(next_body) = bodies.pop() {
+                    let body_eval = StepState::EvalThis(next_body, false);
+                    let watching_state = StepState::EvaluatingLambda {
+                        name: name,
+                        bodies: bodies,
+                    };
+                    ctx.stack.push(watching_state);
+                    ctx.stack.push(body_eval);
+                } else {
+                    ctx.stack.push(StepState::Complete(value));
+                }
+            }
+            StepState::PopEnv => {
+                ctx.env_stack.pop();
+                ctx.stack.push(StepState::Complete(value));
+            }
+            a => panic!("step_eval(..): invalid stack state: [{:?}, {:?}, {:?}]",
+                        ctx.stack, a, StepState::Complete(value))
         }
     } else {
         match top {
             StepState::EvalThis(value, proc_head) => {
                 try!(eval_this(value, ctx, proc_head));
             }
+            StepState::PopEnv => { ctx.env_stack.pop(); },
             StepState::Complete(_) => unreachable!(),
             a@StepState::Return |
-            a@StepState::Lambda {..} |
-            a@StepState::PreEvaluatedCallable { .. } =>
+            a@StepState::ArgCollectingLambda {..} |
+            a@StepState::PreEvaluatedCallable { .. } |
+            a@StepState::EvaluatingLambda { .. } =>
                 panic!("step_eval(..): invalid stack state: [..., {:?}]", a)
         }
     }
@@ -163,9 +212,9 @@ fn eval_this<S: State + ?Sized>(value: Value,
     }
 }
 
-pub fn apply_lambda<S: ?Sized>(procedure: &Procedure,
+pub fn apply_lambda<S: ?Sized>(procedure: Procedure,
                                   args: Vec<Value>,
-                                  ctx: &mut LoadedContext<S>) -> AresResult<Value>
+                                  ctx: &mut LoadedContext<S>) -> AresResult<()>
 where S: State {
     for arg in &args {
         if let &Value::ForeignFn(ForeignFunction { typ: FfType::Ast, .. }) = arg {
@@ -173,19 +222,32 @@ where S: State {
         }
     }
 
-    let mut new_env = try!(procedure.gen_env(args));
-    ctx.with_other_env(&mut new_env, |ctx| {
-        let mut last = None;
-        for body in &*procedure.bodies {
-            // FIXME remove recursive call
-            last = Some(try!(ctx.eval(body)));
-        }
-        // it's impossible to make a lambda without a body
-        Ok(last.unwrap())
-    })
+    let new_env = try!(procedure.gen_env(args));
+    let mut bodies = (*procedure.bodies).clone();
+    bodies.reverse();
+
+    let first_body = bodies.pop().unwrap();
+    let body_eval = StepState::EvalThis(first_body, false);
+
+    ctx.env_stack.push(new_env.clone());
+
+    ctx.stack.push(StepState::PopEnv);
+    if bodies.len() == 0 {
+        // optimizing the common case (lambdas with only one body)
+        ctx.stack.push(body_eval);
+    } else {
+        let watching_state = StepState::EvaluatingLambda {
+            name: procedure.name,
+            bodies: bodies,
+        };
+        ctx.env_stack.push(new_env.clone());
+        ctx.stack.push(watching_state);
+        ctx.stack.push(body_eval);
+    }
+    Ok(())
 }
 
-pub fn apply_function<S: ?Sized>(function: &ForeignFunction<()>,
+pub fn apply_function<S: ?Sized>(function: ForeignFunction<()>,
                                  args: Vec<Value>,
                                  ctx: &mut LoadedContext<S>) -> AresResult<Value>
 where S: State {
@@ -200,17 +262,55 @@ where S: State {
     (corrected.function)(&args[..], ctx)
 }
 
-pub fn apply<'a, S: ?Sized>(func: &Value,
-                            args: Vec<Value>,
-                            ctx: &mut LoadedContext<S>) -> AresResult<Value>
+pub fn do_apply<'a, S: ?Sized>(func: Value, args: Vec<Value>, ctx: &mut LoadedContext<S>)
+-> AresResult<()>
 where S: State {
-    match func.clone() {
+    match func {
         Value::Lambda(procedure, _) => {
-            apply_lambda(&procedure, args, ctx)
+            apply_lambda(procedure, args, ctx)
         }
         Value::ForeignFn(ff) => {
-            apply_function(&ff, args, ctx)
+            let res = try!(apply_function(ff, args, ctx));
+            ctx.stack.push(StepState::Complete(res));
+            Ok(())
         }
         other => Err(AresError::UnexecutableValue(other)),
+    }
+}
+
+impl ::std::fmt::Debug for StepState {
+    fn fmt(&self, formatter: &mut ::std::fmt::Formatter) -> Result<(), ::std::fmt::Error> {
+        match self {
+            &StepState::EvalThis(ref v, _) =>
+                formatter.debug_tuple("EvalThis")
+                         .field(v)
+                         .finish(),
+            &StepState::PopEnv =>
+                formatter.debug_tuple("PopEnv")
+                         .finish(),
+            &StepState::Return =>
+                formatter.debug_tuple("Return")
+                         .finish(),
+            &StepState::Complete(ref v) =>
+                formatter.debug_tuple("Complete")
+                         .field(v)
+                         .finish(),
+            &StepState::ArgCollectingLambda { ref func, ref evaluated, .. } =>
+                formatter.debug_struct("ArgCollectingLambda")
+                         .field("func", func)
+                         .field("evaluated", evaluated)
+                         .field("yet_to_be_evaluated", &"[..]")
+                         .finish(),
+            &StepState::PreEvaluatedCallable { ref unevaluated } =>
+                formatter.debug_struct("PreEvaluatedCallable")
+                         .field("unevaluated", unevaluated)
+                         .finish(),
+            &StepState::EvaluatingLambda { ref bodies, ref name, .. } =>
+                formatter.debug_struct("EvaluatingLambda")
+                         .field("name", name)
+                         .field("bodies", bodies)
+                         .field("env", &"{..}")
+                         .finish()
+        }
     }
 }
